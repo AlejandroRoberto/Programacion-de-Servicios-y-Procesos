f32fcff91c65800c2366225edc7e489d69c6567c
Alexrc_ <73705263+AlejandroRoberto@users.noreply.github.com>
2025-02-19 17:58:54 +0100

    Add files via upload

---
+++ HILOS_EJER_11/HILOS_EJER_11/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Hilo h1 = new Hilo("Hilo 1");
+		
+		Hilo h2 = new Hilo("Hilo 2");
+		
+		Hilo h3 = new Hilo("Hilo 3");
+		
+		
+		h1.start();
+		h2.start();
+		h3.start();
+		
+		System.out.println("3 hilos iniciados");
+	}
+
+}
