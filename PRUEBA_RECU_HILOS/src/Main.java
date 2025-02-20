efcda9f75360d96773caa821649518ef0d6629e8
Alexrc_ <73705263+AlejandroRoberto@users.noreply.github.com>
2025-02-20 13:07:33 +0100

    Update Main.java

--- PRUEBA_RECU_HILOS/src/Main.java
+++ PRUEBA_RECU_HILOS/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		 Juego juego = new Juego(10, 2);  // Tablero de 10 de largo y 2 de ancho
+		 Juego juego = new Juego(11, 2);  // Tablero de 10 de largo y 2 de ancho
 	     juego.iniciarJuego();
 	}
 
